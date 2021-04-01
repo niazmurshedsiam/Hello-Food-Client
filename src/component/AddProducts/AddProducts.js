ddc94fed6ac70397495ceecef5b2dbc34a91c1b1
niazmurshedsiam <niazmurshed16@gmail.com>
2021-04-02 00:52:58 +0600

    Upload events with image link to database

--- src/component/AddProducts/AddProducts.js
+++ src/component/AddProducts/AddProducts.js
@@ -1,18 +1,36 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 const AddProducts = () => {
   const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = (data) => console.log(data);
-  const handleImageUpload = (event) => {
+  const [imageURL,setImageURL] = useState(null);
+  const onSubmit = (data) => {
+      const productData = {
+          name: data.name,
+          imageURL:imageURL
+      };
+      const url = `http://localhost:5000/addProduct`;
+      console.log(productData);
+      console.log(data);
+      fetch(url,{
+          method:'POST',
+          headers:{
+              'content-type':'application/json'
+          },
+          body:JSON.stringify(productData)
+      })
+      .then(res=>res.json())
+      .then(data=>console.log('Server side response'))
+    };
+  const handleImageUpload = event => {
     console.log(event.target.files[0]);
     const imageData = new FormData();
-    imageData.set("key", "335045a2e20dfb20b2e75072ff553f1f");
-    imageData.append("image", event.target.files[0]);
-    axios.post("https://api.imgbb.com/1/upload", 
+    imageData.set("key", '335045a2e20dfb20b2e75072ff553f1f');
+    imageData.append('image', event.target.files[0]);
+    axios.post('https://api.imgbb.com/1/upload', 
     imageData)
       .then(function (response) {
-        console.log(response.data.data.display_url);
+        setImageURL(response.data.data.display_url);
       })
       .catch(function (error) {
         console.log(error);
@@ -22,7 +40,7 @@ const AddProducts = () => {
     <div>
       <h1>Add your awesome Event here</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input name="example" defaultValue="test" ref={register} />
+        <input name="name" defaultValue="New Product" ref={register} />
         <br />
         <input
           name="exampleRequired"
